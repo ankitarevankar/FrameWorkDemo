ecb0f0456eb3d795adef43fe994d3d4cf54c13b8
Ankita <ankitarevankar29@gmail.com>
2024-05-18 10:47:23 +0530

    adde new test

--- SampleProject/src/test/java/pack1/ContactTest.java
+++ SampleProject/src/test/java/pack1/ContactTest.java
@@ -18,9 +18,13 @@ public class ContactTest {
 		System.out.println("execute createContactTest");
 		
 	}
+	
 	@Test
 	public void modifyContactTest() {
 		System.out.println("execute modifyContactTest");
 	}
-
+    @Test
+	public void deleteContactTest() {
+		System.out.println("execute deleteContactTest");
+	}
 }
